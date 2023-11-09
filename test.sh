f2eb53ce3622b583205bafdb37e10187b967c489
liam buchanan <=>
2023-11-09 19:27:55 +0000

    ci/cd update

--- test.sh
+++ test.sh
@@ -1,15 +1,27 @@
 CHANGED_FILES=$(git diff --name-only HEAD~1 HEAD)
+echo $CHANGED_FILES
 TESTED_CONTAINERS=()
 
-# loop through changed files in the ./backend/Containers directory that have Container in their name
 for FILE in $CHANGED_FILES; do
 if [[ $FILE == backend/Containers/*Container* ]]; then
-    CONTAINER=$(echo $FILE | cut -d'/' -f3)
-    if [[ ! " ${TESTED_CONTAINERS[@]} " =~ " ${CONTAINER} " ]]; then
-    cd backend/Containers/$CONTAINER
-    pytest
-    TESTED_CONTAINERS+=($CONTAINER)
-    cd ../../..
-    fi
+        CONTAINER=$(echo $FILE | cut -d'/' -f3)
+        if [[ ! " ${PUSHED_CONTAINERS[@]} " =~ " ${CONTAINER} " ]]; then
+            cd backend/Containers/$CONTAINER
+
+            container_name=$(echo $CONTAINER | sed 's/Container//g' | tr '[:upper:]' '[:lower:]')
+            echo $container_name
+            container_name="timetable-container"
+                        - name: Install jq
+                uses: appleboy/setup-apt@v2
+                with:
+                    update: true
+                    packages: j
+            LATEST_TAG=$(curl -s https://registry.hub.docker.com/v2/repositories/tennisdockerimages/$container_name/tags | jq -r '.results[].name' | grep -oE '^v[0-9]+\.[0-9]+\.[0-9]+$' | sort -V | tail -n1)
+
+            echo $LATEST_TAG
+            NEW_TAG=$(echo $LATEST_TAG | awk -F. -v OFS=. '{$NF = $NF + 1; print}')
+
+            echo $NEW_TAG
+fi
 fi
 done
